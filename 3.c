4596bfa35619720b79297a1062139b328119e288
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-11-10 22:30:13 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,23 +2,40 @@
 #include <stdio.h>
 int main()
 {
-    int n,k,count,h;
+    int n,k,h,t,i;
+    printf("enter no.of test cases:");
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+    int count=0;
     printf("enter  a number:");
     scanf("%d",&n);
     k=n;
     do
     {
+        
         h=n%10;
-        if(k%h==0)
+        if(h==0)
         {
-            count=count+1;
+            n=n/10;
+            continue;
         }
-        n=n/10;
-
+        else
+        {
+            if(k%h==0)
+           {
+               count=count+1;
+               n=n/10;
+           }
+           else 
+           {
+               n=n/10;
+           }
+        
+        }
     }while(n!=0);
-    printf("%d",count);
-
+    printf("%d\n",count);
+    }
 return 0;
     
 }
-
