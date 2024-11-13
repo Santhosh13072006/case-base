d17b870d603e76d8add13e691878f07bfbb1e17b
Santhosh13072006 <santhoshreddy1307@gmail.com>
2024-11-13 15:28:53 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,67 +1,98 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
-    int N,i;
-    printf("enter no.of test cases:");
-    scanf("%d",&N);
-    for(i=0;i<N;i++)
+    int d,f;
+    printf("enter no.of cases:");
+    scanf("%d",&f);
+    for(d=0;d<f;d++)
     {
-        char s[100],a[100];
-        int l,h[100],k[100],j,c,m[100];
-        printf("enter a string\n");
-        scanf("%s",s);
-        l=strlen(s);
-        s[l]='\0';
-        for(j=0;j<l;j++)
-        {
-            h[j]=s[j];
-        }
         
-         
-        for(j=0;j<l;j++)
-        {
-            k[j]=h[l-j-1];
-        }
+    
+    char s[100],c,g[100];
+    int i,l,j,a,b,n=0,m=0,k,h[100],e;
+    printf("enter a string:");
+    scanf("%s",s);
+    l=strlen(s);
+    strcpy(g,s);
+    
+    for(k=l-1;k>=0;k--)
+    {
         
-        for(j=1;j<l;j++)
+    
+         for(i=l-1;i>=k;i--)
         {
-            if(k[0]>k[j])
+             for(j=i-1;j>=k;j--)
             {
-                c=k[j];
-                k[j]=k[0];
-                k[0]=c;
-                break;
+             a=s[i];
+             b=s[j];
+             
+            
+            if(a>b)
+            {
+                
+                 c= s[i];
+                 s[i]=s[j];
+                 s[j]=c;
+                 n=i;
+                 m=j;
+                 
+            
+            break;
                 
             }
+            
+            
+             }
+             if(strcmp(g,s)!=0)
+            {
+                break; 
+            }
+             
         }
-         
-        for(j=0;j<l;j++)
+        if(strcmp(g,s)!=0)
         {
-            m[j]=k[l-j-1];
+            break;
         }
-         
-        for(j=0;j<l;j++)
-        {
-            a[j]=m[j];
-        }
-        a[l]='\0';
-        
-        if(strcmp(s,a)==0)
-        {
-            printf("no answer");
-        }
-        else
-        {
-            printf("lexographically bigger string is \n");
-            for(j=0;j<l;j++)
-        {
-            printf("%c",a[j]);
-        }
-        }
-        printf("\n");
     }
+     
+    for(i=m+1;i<l;i++)
+    {
+        int a=s[i];
+        h[i]=a;
+    }
+    for(i=m+1;i<l;i++)
+    {
+        for(j=i+1;j<l;j++)
+        {
+            if(h[i]>h[j])
+            {
+                e=h[i];
+                h[i]=h[j];
+                h[j]=e;
+            }
+        }
+    }
+     for(i=m+1;i<l;i++)
+    {
+        char d=h[i];
+        s[i]=d;
+    }
+    
+
+    
+    if(strcmp(g,s)!=0)
+    { 
+      printf("bigger string is %s\n",s);
+    }
+    else
+    {
+        printf("no answer\n");
+    }
+    }
+    
 
     return 0;
 }
